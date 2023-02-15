ffb42e5f6fefa6670fefe7e742bcf6e217fce7f7
anic4cio <anicaciovinicius36@gmail.com>
2023-02-15 15:41:32 -0300

    refactor: dirCompresser function better writen

--- src/dirCompresser.ts
+++ src/dirCompresser.ts
@@ -1,18 +1,36 @@
 import archiver from 'archiver'
 import { createWriteStream } from 'fs'
 
+const delay = async (milliseconds: number) => {
+  return new Promise<void>(resolve => {
+    setTimeout(() => resolve(), milliseconds)
+  })
+}
+
 export default async (path: string) => {
-  const output = createWriteStream(`${path}.zip`)
+  const output = createWriteStream('invoices.zip')
   const archive = archiver('zip', { zlib: { level: 9 } })
 
   output.on('close', () => {
     console.log(`Total .zip file size: ${Math.round(archive.pointer() / 1024)}KB`)
     console.log('archiver has been finalized and the output file instance has closed.')
   })
+  
+  archive.on('warning', (err) => {
+    if (err.code === 'ENOENT') {
+      console.log(`Directory not found: ${err}`)
+    } else {
+      throw err;
+    }
+  })
+  
+  archive.on('error', (err) => {
+    throw err
+  })
 
-  archive.on('error', (err) => { throw err })
   archive.pipe(output)
   archive.directory(path, false)
-  archive.directory('subdir', 'new-subdir')
   archive.finalize()
+  await delay(3000)
+  return
 }
